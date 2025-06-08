d465f8a826206d0da35bce1edfdecc53d3d89258
MrSotto665 <mrsotto071@gmail.com>
2025-06-09 00:16:55 +0600

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # Example: https://yourdomain.com/webhook
 CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME")  # Example: '@YourChannelUsername'
 CHANNEL_USERNAME1 = os.getenv("CHANNEL_USERNAME1")
-MESSAGE_TEXT = "এখানে সবাই চ্যাট করে, সবাই একটিভ থাকে। এখানেই মজা, এখানেই আড্ডা! 💬🔥/n নিচের লিংকে গিয়ে চ্যাট করো। 💬👇 /n
+MESSAGE_TEXT = "এখানে সবাই চ্যাট করে, সবাই একটিভ থাকে। এখানেই মজা, এখানেই আড্ডা! 💬🔥\n নিচের লিংকে গিয়ে চ্যাট করো। 💬👇 \n
 https://t.me/MakefriendsglobalBot/Letschat"
 
 app = FastAPI()
